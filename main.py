e55b278595fd57f9d36f646a1f66205a58ec23aa
BJULIEN <julien.brousse@ynov.com>
2025-02-28 11:29:11 +0100

    Upgrade the game

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from types_plantes.fleur import Fleur
 from types_plantes.legume import Legume
 from types_plantes.arbre import Arbre
 
+
 def main():
     mon_jardin = Jardin()
 
@@ -14,51 +15,59 @@ def main():
     mon_jardin.ajouter_plante(tomate)
     mon_jardin.ajouter_plante(chene)
 
-    # Interaction avec les plantes
-    mon_jardin.interagir_avec_plante("Rose", "arroser", 60)
     while True:
         print("\nActions disponibles :")
         print("1. Arroser une plante")
         print("2. Fertiliser une plante")
         print("3. Entretenir une plante")
         print("4. Afficher l'état du jardin")
-        print("6. Ajouter une plante")
-        print("5. Quitter")
+        print("5. Ajouter une plante")
+        print("6. Générer un événement aléatoire")
+        print("7. Quitter")
 
-        choix = input("Choisissez une action (1-6) : ")
+        choix = input("Choisissez une action (1-7) : ")
 
         if choix == "1":
             plante = input("Entrez le nom de la plante à arroser : ")
             quantite = int(input("Entrez la quantité d'eau : "))
             mon_jardin.interagir_avec_plante(plante, "arroser", quantite)
+
         elif choix == "2":
             plante = input("Entrez le nom de la plante à fertiliser : ")
             mon_jardin.interagir_avec_plante(plante, "fertiliser")
+
         elif choix == "3":
             plante = input("Entrez le nom de la plante à entretenir : ")
             mon_jardin.interagir_avec_plante(plante, "entretenir")
+
         elif choix == "4":
             mon_jardin.afficher_jardin()
+
         elif choix == "5":
-            print("Merci d'avoir joué !")
-        elif choix == "6":
             print("Plantes disponibles à ajouter :")
             print("1. Rose (Fleur)")
             print("2. Tomate (Légume)")
             print("3. Chêne (Arbre)")
-            print("4. Cerisier (Arbre)")
+
             type_plante = input("Choisissez une plante à ajouter (1-3) : ")
             if type_plante == "1":
                 mon_jardin.ajouter_plante(Fleur("Rose", 50, 3, 0.5, "Rouge"))
                 print("Rose ajoutée avec succès.")
+
             elif type_plante == "2":
                 mon_jardin.ajouter_plante(Legume("Tomate", 60, 4, 0.7, 45))
                 print("Tomate ajoutée avec succès.")
+
             elif type_plante == "3":
                 mon_jardin.ajouter_plante(Arbre("Chêne", 100, 5, 0.3, 20))
                 print("Chêne ajoutée avec succès.")
+
             else:
                 print("Choix invalide, veuillez réessayer.")
+
+        elif choix == "7":
+            print("Merci d'avoir joué !")
+            break
         else:
             print("Choix invalide, veuillez réessayer.")
 
