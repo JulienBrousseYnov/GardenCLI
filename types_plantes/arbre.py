d1a3d09e47886b4751b30c08b900dce34d524bdc
BJULIEN <julien.brousse@ynov.com>
2025-03-03 11:46:07 +0100

    Change value for sante

--- types_plantes/arbre.py
+++ types_plantes/arbre.py
@@ -10,21 +10,18 @@ class Arbre(Plante):
         print(f"{self.nom} - Santé: {self.sante} - Hauteur maximale: {self.hauteur_max}m")
 
     def entretenir(self):
-        # Augmenter la santé de l'arbre de 10 à chaque entretien
         self.sante += 10
         if self.sante > 100:
-            self.sante = 250  # S'assurer que la santé ne dépasse pas 100
+            self.sante = 250 
         print(f"Entretenir {self.nom} - Santé après entretien: {self.sante}")
 
     def arroser(self, quantite):
-    # Augmenter la santé de la fleur de 5 pour chaque unité d'eau
         self.sante += + quantite
         if self.sante > 100:
             self.sante = 250
         print(f"Arroser {self.nom} - Santé après arrosage: {self.sante}")
 
     def fertiliser(self):
-        # Augmenter la santé de l'arbre de 20 à chaque fertilisation
         self.sante += 20
         if self.sante > 100:
             self.sante = 250
--- types_plantes/legume.py
+++ types_plantes/legume.py
@@ -10,21 +10,18 @@ class Legume(Plante):
         print(f"{self.nom} - Santé: {self.sante} - Temps de croissance: {self.temps_croissance} jours")
 
     def entretenir(self):
-        # Augmenter la santé du légume de 10 à chaque entretien
         self.sante += 10
-        if self.sante > 100:
-            self.sante = 100  # S'assurer que la santé ne dépasse pas 100
+        if self.sante > 50:
+            self.sante = 50
         print(f"Entretenir {self.nom} - Santé après entretien: {self.sante}")
 
     def arroser(self, quantite):
-    # Augmenter la santé de la fleur de 5 pour chaque unité d'eau
         self.sante += + quantite
         if self.sante > 50:
-            self.sante = 50  # S'assurer que la santé ne dépasse pas 100
+            self.sante = 50
         print(f"Arroser {self.nom} - Santé après arrosage: {self.sante}")
 
     def fertiliser(self):
-        # Augmenter la santé du légume de 20 à chaque fertilisation
         self.sante += 20
         if self.sante > 50:
             self.sante = 50
